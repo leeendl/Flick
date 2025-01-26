9e2e6947011bd19bf72b8b73995005d2f612f0f3
LeeEndl <gavinolson71@gmail.com>
2025-01-25 21:30:17 -0600

    add textField()

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,14 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.*
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
-    private var kana = arrayOf("きょう", "ともだち", "ひる") // ...
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -20,19 +26,38 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    val reKana = kana.random()
-                    Box {
+                    val kana = listOf("きょう", "ともだち", "ひる") // ...
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        var reKana by remember { mutableStateOf(kana.random()) }
                         Text(
-                            text = reKana, fontSize = 60.sp,
+                            text = reKana, fontSize = 60.sp, // @todo
                             modifier = Modifier
-                                .offset(
-                                    360.dp,
-                                    160.dp
-                                )
-                                .border(3.dp, Color.DarkGray)
+                                .border(3.dp, Color.DarkGray, shape = RoundedCornerShape(8.dp))
                                 .padding(9.dp)
                         )
-
+                        var userInput by remember { mutableStateOf("") }
+                        TextField(
+                            value = userInput,
+                            onValueChange = {
+                                userInput = it
+                                if (it == reKana) {
+                                    reKana = kana.random()
+                                    userInput = ""
+                                }
+                            },
+                            textStyle = TextStyle(fontSize = 30.sp), // @todo
+                            shape = RoundedCornerShape(8.dp),
+                            modifier = Modifier
+                                .padding(26.dp),
+                            colors = TextFieldDefaults.colors(
+                                unfocusedIndicatorColor = Color.Transparent,
+                                focusedIndicatorColor = Color.Transparent
+                            )
+                        )
                     }
                 }
             }
