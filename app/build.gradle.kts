9188ccba9d4fcbdba1697abfaf7d637187fa7f4d
LeeEndl <gavinolson71@gmail.com>
2025-01-29 20:33:03 -0600

    updated composeBom to 2025.01.01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.myapplication"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "b10"
+        versionCode = 11
+        versionName = "b11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
