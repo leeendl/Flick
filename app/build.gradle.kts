f997b7266fbc519fd2fd19140155e59bb7fbef4c
LeeEndl <gavinolson71@gmail.com>
2025-02-14 16:52:25 -0600

    updated composeBom to 2025.02.00

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "app.flick"
         minSdk = 26
         targetSdk = 35
-        versionCode = 27
-        versionName = "0.27"
+        versionCode = 28
+        versionName = "0.28"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
